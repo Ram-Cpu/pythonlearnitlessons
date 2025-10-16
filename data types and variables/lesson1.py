ecaae74494765e92e36c9fe64c800f52b6198624
Ram-Cpu <ramasainaga8@gmail.com>
2025-10-17 00:16:03 +0530

    leeson1

---
+++ data types and variables/lesson1.py
@@ -0,0 +1,21 @@
+#using double quate
+print("hello world")
+
+#using single quate
+print('hello\'s world')
+
+#using lines
+print("Hello world")
+print("Hello world1")
+print("Hello world2")
+
+#line breaking using \n
+print("Hello world1\n Hello world2\n Hello world3")
+
+#using triple quates
+print("""this is lin1.
+this is line2
+this is line3""")
+
+
+
